43de9d4a981fb1c80da43cb4908e83c99979d5c1
Anshuman Giramkar <99130545+AnshumanGiramkar@users.noreply.github.com>
2024-04-25 23:16:58 +0530

    Update SF90Stradale.jsx

--- src/pages/SF90Stradale.jsx
+++ src/pages/SF90Stradale.jsx
@@ -22,17 +22,6 @@ export default function SF90Stradale() {
           featureItems={featureItems}
         />
       </section>
-      {/* <section className="section">
-        <ImageBackgroundNoText backgroundImage={teslaChargerImage} />
-      </section>
-      <section className="section">
-        <ImageGrid
-          image1={cybertruckImage}
-          image2={teslaChargerImage}
-          text1="Stay Connected - Instantly connect with multi-device Bluetooth, or fast charge devices with wireless and 36-watt USB-C charging."
-          text2="Sublime Sound - A 22-speaker, 960-watt audio system with Active Road Noise Reduction offers the best listening experience wherever you are."
-        />
-      </section> */}
     </div>
   );
 }
